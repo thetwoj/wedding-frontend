4635ad3a1c67673629341fdb834fcf86ea01deef
JJ Graham <jgraham@indeed.com>
2018-04-03 19:45:57 -0500

    Beginnings of RSVP page, retrieval by access code and stores invitation to state

--- src/public/rsvp/Rsvp.react.js
+++ src/public/rsvp/Rsvp.react.js
@@ -3,9 +3,59 @@ import React, {Component} from 'react';
 import '../../App.css';
 import Admin from "../../admin/Admin.react";
 
+import FlatButton from 'material-ui/FlatButton';
+import TextField from 'material-ui/TextField';
+
 class Rsvp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      accessCode: '',
+      invitation: null,
+    };
+  }
+
+  getRsvp = (event) => {
+    fetch('/api/rsvp/' + this.state.accessCode, {
+      method: 'get',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    })
+      .then(resp => {
+        if (!resp.ok) {
+          throw Error();
+        }
+        return resp
+      })
+      .then(resp => resp.json())
+      .then(resp => this.setState({invitation: resp}))
+      .catch();
+
+    event.preventDefault();
+  };
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({[name]: value});
+  };
+
   render() {
-    return <span>hello</span>
+    return (
+      <div className="fullWidth">
+        <TextField
+          name="accessCode"
+          onChange={this.handleInputChange}
+          floatingLabelText="Code"
+        />
+        <FlatButton label="Submit" onClick={this.getRsvp}/>
+      </div>
+    )
   }
 }
 
